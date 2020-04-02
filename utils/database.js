c64b1fc93eff7edea3f8087b79b778668fcf1041
William Salvucci <wsalvucci@outlook.com>
2020-04-02 01:53:57 -0400

    Adding parameter to mongo database

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const app = require('./expressModule')
 
-const url = 'mongodb+srv://thelounge_app_2020:' + process.env.DATABASE_PASSWORD + '@loungebotcluster-7wuhd.mongodb.net/test?retryWrites=true&w=majority'
+const url = 'mongodb+srv://thelounge_app_2020:' + process.env.DATABASE_PASSWORD + '@loungebotcluster-7wuhd.mongodb.net/test?retryWrites=true&w=majority&authSource=admin'
 
 MongoClient.connect(url, function(err, client) {
     if (err) throw err
