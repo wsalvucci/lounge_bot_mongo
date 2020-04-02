5e81705cba8e7528dfbad593b2fc8ee056ade4e9
William Salvucci <wsalvucci@outlook.com>
2020-04-02 01:36:18 -0400

    Trying to fix deployment errors

--- bot.js
+++ bot.js
@@ -18,10 +18,6 @@ client.registry
     .registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'))
 
-client.setProvider(
-    sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
-).catch(console.error);
-
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
 	client.user.setActivity('with Commando');
